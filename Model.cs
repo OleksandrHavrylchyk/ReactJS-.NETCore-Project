1c6724743f4818ac55c155c4138fb4ddffb9f4b8
OleksandrHavrylchyk <aleksandr.havrylchyk@gmail.com>
2020-01-28 17:53:52 +0200

    Created react/.netcore application

---
+++ Model.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace WebApplication
+{
+    public class ModelContext : DbContext
+    {
+        public ModelContext(DbContextOptions<ModelContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Model> Model{ get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+        => options.UseSqlite("Data Source=database.db");
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Model>().HasData(new Model { Id = 1, Title = "Title 1", Content = "Content 1" });
+        }
+}
+    public class Model
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Content { get; set; }
+
+    }
+}
