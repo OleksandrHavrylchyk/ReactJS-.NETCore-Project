928abee52e3afd5ffad5af50d51527fb9ae25dcd
OleksandrHavrylchyk <aleksandr.havrylchyk@gmail.com>
2020-01-29 18:09:36 +0200

    Added models Product, Category, created controller for getting all product from db(not final version)

---
+++ Controllers/ProductsController.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using WebApplication;
+
+namespace WebApplication.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        private readonly ModelContext _context;
+
+        public ProductsController(ModelContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Products
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Products>>> GetProduct()
+        {
+            return await (from product in _context.Product
+                                 join category in _context.Category on product.CategoryID equals category.ID into tmp
+                                 from m in tmp.DefaultIfEmpty()
+
+                                 select new Products
+                                 {
+                                     ID = product.ID,
+                                     ProductName = product.ProductName,
+                                     Description = product.Description,
+                                     CategoryID = product.CategoryID,
+                                     Category = m,
+                                 }
+            ).ToListAsync();
+            //return await _context.Product.ToListAsync();
+        }
+
+        // GET: api/Products/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Products>> GetProducts(int id)
+        {
+            var products = await _context.Product.FindAsync(id);
+
+            if (products == null)
+            {
+                return NotFound();
+            }
+
+            return products;
+        }
+
+        // PUT: api/Products/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for
+        // more details see https://aka.ms/RazorPagesCRUD.
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutProducts(int id, Products products)
+        {
+            if (id != products.ID)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(products).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!ProductsExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Products
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for
+        // more details see https://aka.ms/RazorPagesCRUD.
+        [HttpPost]
+        public async Task<ActionResult<Products>> PostProducts(Products products)
+        {
+            _context.Product.Add(products);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetProducts", new { id = products.ID }, products);
+        }
+
+        // DELETE: api/Products/5
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<Products>> DeleteProducts(int id)
+        {
+            var products = await _context.Product.FindAsync(id);
+            if (products == null)
+            {
+                return NotFound();
+            }
+
+            _context.Product.Remove(products);
+            await _context.SaveChangesAsync();
+
+            return products;
+        }
+
+        private bool ProductsExists(int id)
+        {
+            return _context.Product.Any(e => e.ID == id);
+        }
+    }
+}
---
+++ Product.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace WebApplication
+{
+    public class ModelContext : DbContext
+    {
+        public ModelContext(DbContextOptions<ModelContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Products> Product{ get; set; }
+        public DbSet<Categories> Category { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+        => options.UseSqlite("Data Source=database.db");
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Categories>().HasData(new Categories { ID = 1, CategoryName = "Category 1" });
+            modelBuilder.Entity<Categories>().HasData(new Categories { ID = 2, CategoryName = "Category 2" });
+            modelBuilder.Entity<Categories>().HasData(new Categories { ID = 3, CategoryName = "Category 3" });
+            modelBuilder.Entity<Categories>().HasData(new Categories { ID = 4, CategoryName = "Category 4" });
+            modelBuilder.Entity<Categories>().HasData(new Categories { ID = 5, CategoryName = "Category 5" });
+            modelBuilder.Entity<Products>().HasData(new Products { ID = 1, ProductName = "Title 1", Description = "Content 1" , CategoryID = 1 });
+            modelBuilder.Entity<Products>().HasData(new Products { ID = 2, ProductName = "Title 2", Description = "Content 2" , CategoryID = 2 });
+            modelBuilder.Entity<Products>().HasData(new Products { ID = 3, ProductName = "Title 3", Description = "Content 3" , CategoryID = 3 });
+            modelBuilder.Entity<Products>().HasData(new Products { ID = 4, ProductName = "Title 4", Description = "Content 4" , CategoryID = 4 });
+            modelBuilder.Entity<Products>().HasData(new Products { ID = 5, ProductName = "Title 5", Description = "Content 5" , CategoryID = 5 });
+            modelBuilder.Entity<Products>().HasData(new Products { ID = 6, ProductName = "Title 6", Description = "Content 6" , CategoryID = 1 });
+            modelBuilder.Entity<Products>().HasData(new Products { ID = 7, ProductName = "Title 7", Description = "Content 7" , CategoryID = 2 });
+            modelBuilder.Entity<Products>().HasData(new Products { ID = 8, ProductName = "Title 8", Description = "Content 8" , CategoryID = 3 });
+            modelBuilder.Entity<Products>().HasData(new Products { ID = 9, ProductName = "Title 9", Description = "Content 9" , CategoryID = 5 });
+            modelBuilder.Entity<Products>().HasData(new Products { ID = 10, ProductName = "Title 10", Description = "Content 10", CategoryID = 4 });
+        }
+    }
+    public class Products
+    {
+        public int ID { get; set; }
+        [StringLength(50)]  
+        public string ProductName { get; set; }
+        [StringLength(100)]
+        public string Description { get; set; }
+        public int CategoryID { get; set; }
+        [ForeignKey("CategoryID")]
+        public Categories Category { get; set; }
+
+    }
+
+    public class Categories
+    {
+        public int ID { get; set; }
+        [StringLength(30)]
+        public string CategoryName { get; set; }
+    }
+}
